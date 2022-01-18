28980174c7b1a5d39f9009b292c74f807102119f
unknown <madhusureshnc@gmail.com>
2022-01-18 14:58:19 -0500

    Edited

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Write your JavaScript code here!
-
 window.addEventListener("load", function() {
     let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (json) {
@@ -17,63 +15,24 @@ window.addEventListener("load", function() {
         let fuelStatus = document.getElementById('fuelStatus'); 
         let launchStatus = document.getElementById('launchStatus'); 
       
-        if (pilotName.value == ""|| copilotName.value == "" || fuelLevel.value == "" || cargoMass.value == "") { 
-            alert("All fields are required!"); 
-            event.preventDefault(); 
-        }
-        if (isNaN(pilotName.value) || isNaN(copilotName.value)){ 
-            pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready`; 
-            copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready`; 
-            } 
-            else{ 
-           alert("Pilot & Co-pilot need to be a valid name"); 
-           event.preventDefault(); 
-             } 
-           if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)){ 
-            alert("Invalid Fuel level & cargo mass value"); 
-            event.preventDefault(); 
-            } 
-           else{ 
-            if(fuelLevel.value < 10000){ //  
-           fuelStatus.innerHTML = `Fuel level too low for Launch`; 
-           launchStatus.innerHTML = `Shuttle not ready for launch`; 
-               }else{ 
-               fuelStatus.innerHTML = `Fuel level high enough for launch`; 
-               } 
-               
-               if(cargoMass.value > 10000){  
-               cargoStatus.innerHTML = `Cargo mass too heavy for Launch`; 
-               launchStatus.innerHTML = `Shuttle not ready for launch`; 
-               }else{ 
-               cargoStatus.innerHTML = `Cargo mass low enough for launch`; 
-               } 
-               if(fuelLevel.value >= 10000 && cargoMass.value <= 10000) {  
-               launchStatus.innerHTML = `Shuttle is ready for launch`; 
-               fuelStatus.innerHTML = `Fuel level high enough for launch`; 
-               cargoStatus.innerHTML = `Cargo mass low enough for launch`; 
-               } 
-            }
-    
-
-        fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) { 
-        response.json().then(function(json){ 
-        const missionTarget = document.getElementById('missionTarget'); 
-        const index = Math.floor(Math.random() * json.length - 1); 
-        missionTarget.innerHTML = ` 
-        <ol> 
-        <li>Name: ${json[index].name}</li> 
-        <li>Diameter: ${json[index].diameter}</li> 
-        <li>Star: ${json[index].star}</li> 
-        <li>Distance from Earth: ${json[index].distance}</li> 
-        <li>Number of Moons: ${json[index].moons}</li> 
-        </ol> 
-        <img src="${json[index].image}"> 
-        `; 
-        }); 
-        }); 
-        event.preventDefault(); 
-        
+        formSubmission(document,list,pilot,copilot,fuelLevel,cargoLevel);
     });
+    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) { 
+    response.json().then(function(json){ 
+    const missionTarget = document.getElementById('missionTarget'); 
+    missionTarget.innerHTML = ` 
+    <ol> 
+    <li>Name: ${json[index].name}</li> 
+    <li>Diameter: ${json[index].diameter}</li> 
+    <li>Star: ${json[index].star}</li> 
+    <li>Distance from Earth: ${json[index].distance}</li> 
+    <li>Number of Moons: ${json[index].moons}</li> 
+    </ol> 
+    <img src="${json[index].image}"> 
+    `; 
+    }); 
+    }); 
+    event.preventDefault(); 
+
 
     });
- event.preventDefault();
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,5 +1,8 @@
-// Write your helper functions here!
-require('isomorphic-fetch');
+try {
+    require('isomorphic-fetch');
+} catch (e) {
+
+}
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
@@ -16,11 +19,57 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
 }
 
-function validateInput(testInput) {
 
+
+function validateInput(testInput) {
+    if (testInput == '') {
+        return 'Empty';
+    } else if (isNaN(Number(testInput))) {
+        return 'Not a Number';
+    } else {
+        return 'Is a Number';
+    }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    if (pilotName.value == ""|| copilotName.value == "" || fuelLevel.value == "" || cargoMass.value == "") { 
+        alert("All fields are required!"); 
+        event.preventDefault(); 
+    }
+    if (isNaN(pilotName.value) || isNaN(copilotName.value)){ 
+        pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready`; 
+        copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready`; 
+        } 
+        else{ 
+       alert("Pilot & Co-pilot need to be a valid name"); 
+       event.preventDefault(); 
+         } 
+       if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)){ 
+        alert("Invalid Fuel level & cargo mass value"); 
+        event.preventDefault(); 
+        } 
+       else{ 
+        if(fuelLevel.value < 10000){ //  
+       fuelStatus.innerHTML = `Fuel level too low for Launch`; 
+       launchStatus.innerHTML = `Shuttle not ready for launch`; 
+           }else{ 
+           fuelStatus.innerHTML = `Fuel level high enough for launch`; 
+           } 
+           
+           if(cargoMass.value > 10000){  
+           cargoStatus.innerHTML = `Cargo mass too heavy for Launch`; 
+           launchStatus.innerHTML = `Shuttle not ready for launch`; 
+           }else{ 
+           cargoStatus.innerHTML = `Cargo mass low enough for launch`; 
+           } 
+           if(fuelLevel.value >= 10000 && cargoMass.value <= 10000) {  
+           launchStatus.innerHTML = `Shuttle is ready for launch`; 
+           fuelStatus.innerHTML = `Fuel level high enough for launch`; 
+           cargoStatus.innerHTML = `Cargo mass low enough for launch`; 
+           } 
+           list.style.visibility = "visible";
+
+        }
 }
 
 async function myFetch() {
@@ -31,12 +80,17 @@ async function myFetch() {
     return planetsReturned;
 }
 
-function pickPlanet(planets) {
 
+function pickPlanet(planets) {
+    const index = Math.floor(Math.random() * planets.length);
+    return planets[index];
 }
+
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet; 
 module.exports.myFetch = myFetch;
+   
+   
